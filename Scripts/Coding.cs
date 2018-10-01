56e9aeac9f21136a9f7d5628139ed5c78ce41a1c
Mohammad Jamalianpour <jamalianm21@yahoo.com>
2018-10-01 13:58:55 +0330

    Add Coding script

---
+++ Scripts/Coding.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace security
+{
+    public class Coding
+    {
+        public string Md5Sum(string strToEncrypt)
+        {
+            UTF8Encoding ue = new UTF8Encoding();
+            byte[] bytes = ue.GetBytes(strToEncrypt);
+
+            
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] hashBytes = md5.ComputeHash(bytes);
+
+            
+            string hashString = "";
+
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                hashString += System.Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
+            }
+
+            return hashString.PadLeft(32, '0');
+        }
+
+        public string Sha1Sum(string strToEncrypt)
+        {
+            UTF8Encoding ue = new UTF8Encoding();
+            byte[] bytes = ue.GetBytes(strToEncrypt);
+
+            
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            byte[] hashBytes = sha.ComputeHash(bytes);
+
+            
+            string hashString = "";
+
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                hashString += Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
+            }
+
+            return hashString.PadLeft(32, '0');
+        }
+
+    }
+}
