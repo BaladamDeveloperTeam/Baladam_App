ac3231b8e11a09a718d011d7c3b2baf19f3822a0
Mohammad Jamalianpour <jamalianm21@yahoo.com>
2018-10-07 23:06:09 +0330

    Search Code

--- Scripts/Search_panel_Handler.cs
+++ Scripts/Search_panel_Handler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UPersian.Components;
 using UPersian.Utils;
 
@@ -8,17 +9,47 @@ public class Search_panel_Handler : MonoBehaviour
 {
 
     public RtlText PlaceHolder;
+    private readonly string masterKey = "$2y$10$ooZRpgP3iGc6qYju9/03W.34alpAopQ7frXimfKEloqRdvXibbNem";
+    private readonly string Url = "http://127.0.0.2:81/api/GetLiperosal/This_is_PaSSWord_45M127*22";
 
-	void Start ()
-    {
-        //StartCoroutine(Type());
-	}
-	
+    public RtlText ErrorText;
+    public InputField Searchtext;
+    public CategoryInfo[] Categoryinfo;
+    private string SearchResult = "";
 
-	void Update ()
+    private WWWForm SendData()
     {
-		
-	}
+        WWWForm web = new WWWForm();
+        web.AddField("Master", masterKey);
+        web.AddField("Chooser", 7);
+        web.AddField("key", Searchtext.text);
+        return web;
+    }
+
+    private IEnumerator DoSearch()
+    {
+        WWWForm WebGet = SendData();
+        WWW data = new WWW(Url, WebGet);
+        yield return data;
+
+        Debug.Log(data.text);
+        if (data.text != "[]" && data.text != "[ ]" && data.text != null && data.text != "")
+        {
+            SearchResult = data.text;
+
+            Categoryinfo = JsonHelper.FromJson<CategoryInfo>("{\"Items\": " + SearchResult + "}");
+        }
+
+        if (data.text == "bad post" || data.text == "" || data.text == null || data.text.Contains("<!DOCTYPE html>"))
+           ErrorText.gameObject.SetActive(true);
+
+    }
+
+    public void SearchTextUpdate()
+    {
+        if(Searchtext.text.Length >= 2)
+            StartCoroutine(DoSearch());
+    }
 
     IEnumerator Type()
     {
@@ -46,9 +77,5 @@ public class Search_panel_Handler : MonoBehaviour
         PlaceHolder.text += "ﻮ";
         yield return new WaitForSeconds(0.2f);
         PlaceHolder.text += "ﻧ";
-
-
-
-
     }
 }
