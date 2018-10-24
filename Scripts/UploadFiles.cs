015e89304e884cbb030d569d58d4b5ece93d845b
Mohammad Jamalianpour <jamalianm21@yahoo.com>
2018-10-24 15:26:14 +0330

    Upload File Script

---
+++ Scripts/UploadFiles.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using UnityEngine;
+
+public class UploadFiles
+{
+    private readonly string FTPHost = "";
+    private readonly string FTPUserName = "";
+    private readonly string FTPPassword = "";
+
+    public void UploadFile(string FilePath)
+    {
+        try
+        {
+            Debug.Log("Path: " + FilePath);
+
+            WebClient client = new WebClient();
+            Uri uri = new Uri(FTPHost + "/" + new FileInfo(FilePath).Name);
+
+            client.UploadProgressChanged += new UploadProgressChangedEventHandler(OnFileUploadProgressChanged);
+            client.UploadFileCompleted += new UploadFileCompletedEventHandler(OnFileUploadCompleted);
+            client.Credentials = new NetworkCredential(FTPUserName, FTPPassword);
+            client.UploadFileAsync(uri, "STOR", FilePath);
+        }
+        catch
+        {
+            Debug.Log("Upload Error!!!");
+        }
+        
+    }
+
+    void OnFileUploadProgressChanged(object sender, UploadProgressChangedEventArgs e)
+    {
+        Debug.Log("Uploading Progreess: " + e.ProgressPercentage);
+    }
+
+    void OnFileUploadCompleted(object sender, UploadFileCompletedEventArgs e)
+    {
+        Debug.Log("File Uploaded");
+    }
+}
