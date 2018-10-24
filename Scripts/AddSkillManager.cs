750089f06e23f8e3572e778d618a98271674e931
Mohammad Jamalianpour <jamalianm21@yahoo.com>
2018-10-24 12:05:40 +0330

    Optimize Array and Json For SkillCreator

--- Scripts/AddSkillManager.cs
+++ Scripts/AddSkillManager.cs
@@ -83,11 +83,34 @@ public class AddSkillManager : MonoBehaviour
     private void SetSkillPointsParametr()
     {
         SkillPoints = GameObject.FindGameObjectsWithTag("SkillPoints");
+        SkillPointsPath = new string[SkillPoints.Length];
         int a1 = 0, a2 = 0, a3 = 0;
         for (int i = 0; i < SkillPoints.Length; i++)
         {
             SkillPointsPath[i] = GetGameObjectPath(SkillPoints[i]);
         }
+        for (int i = 0; i < SkillPoints.Length; i++)
+        {
+            if (SkillPointsPath[i].Contains("SkillBox") && !SkillPointsPath[i].Contains("SkillBox(1)") && !SkillPointsPath[i].Contains("SkillBox(2)"))
+            {
+                //UserSkill[0].SkillPoints[0].SkillPoints[a1] = SkillPoints[i].gameObject.GetComponent<InputField>().text;
+                a1++;
+            }
+            if (SkillPointsPath[i].Contains("SkillBox(1)"))
+            {
+                //UserSkill[0].SkillPoints[1].SkillPoints[a2] = SkillPoints[i].gameObject.GetComponent<InputField>().text;
+                a2++;
+            }
+            if (SkillPointsPath[i].Contains("SkillBox(2)"))
+            {
+                //UserSkill[0].SkillPoints[2].SkillPoints[a3] = SkillPoints[i].gameObject.GetComponent<InputField>().text;
+                a3++;
+            }
+        }
+        UserSkill[0].SkillPoints[0].SkillPoints = new string[a1];
+        UserSkill[0].SkillPoints[1].SkillPoints = new string[a2];
+        UserSkill[0].SkillPoints[2].SkillPoints = new string[a3];
+        a1 = 0; a2 = 0; a3 = 0;
         for (int i = 0; i < SkillPoints.Length; i++)
         {
             if (SkillPointsPath[i].Contains("SkillBox") && !SkillPointsPath[i].Contains("SkillBox(1)") && !SkillPointsPath[i].Contains("SkillBox(2)"))
@@ -106,7 +129,7 @@ public class AddSkillManager : MonoBehaviour
                 a3++;
             }
         }
-        for(int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)
         {
             int.TryParse(Cost[i].gameObject.GetComponent<InputField>().text, out UserSkill[0].SkillPoints[i].SkillCost);
             int.TryParse(Period[i].gameObject.GetComponent<InputField>().text, out UserSkill[0].SkillPoints[i].SkillPeriod);
@@ -144,5 +167,6 @@ public class AddSkillManager : MonoBehaviour
 
         Debug.Log(data.text);
         SubCategoryJson = data.text;
+        Debug.Log(JsonHelper.ToJson(UserSkill));
     }
 }
