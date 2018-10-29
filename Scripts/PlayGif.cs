3f8754b994dcf34afdb44a213f702fff66ecac14
Mohammad Jamalianpour <jamalianm21@yahoo.com>
2018-10-29 18:14:00 +0330

    Now App Support Gif

---
+++ Scripts/PlayGif.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayGif : MonoBehaviour
+{
+
+    public Sprite[] SpriteImages;
+    public Image ShowPlace;
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+        ShowPlace.sprite = SpriteImages[(int)(Time.time * 10) % SpriteImages.Length];
+	}
+}
