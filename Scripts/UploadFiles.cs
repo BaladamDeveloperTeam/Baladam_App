cc5b43952b1189efb7dda107c8dbfb421b719e50
Mohammad Jamalianpour <jamalianm21@yahoo.com>
2018-11-01 10:31:15 +0330

    Fix UploadFile

--- Scripts/UploadFiles.cs
+++ Scripts/UploadFiles.cs
@@ -4,13 +4,15 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class UploadFiles
 {
-    private readonly string FTPHost = "";
-    private readonly string FTPUserName = "";
-    private readonly string FTPPassword = "";
+    private readonly string FTPHost = "ftp://138.201.32.126";
+    private readonly string FTPUserName = "mohadair";
+    private readonly string FTPPassword = "@Amir22102210";
+    private readonly string SaveTo = "/BaladamSkillImage/";
 
     public void UploadFile(string FilePath)
     {
@@ -19,7 +21,7 @@ public class UploadFiles
             Debug.Log("Path: " + FilePath);
 
             WebClient client = new WebClient();
-            Uri uri = new Uri(FTPHost + "/" + new FileInfo(FilePath).Name);
+            Uri uri = new Uri(FTPHost + SaveTo + "User_" + new FileInfo(FilePath).Name);
 
             client.UploadProgressChanged += new UploadProgressChangedEventHandler(OnFileUploadProgressChanged);
             client.UploadFileCompleted += new UploadFileCompletedEventHandler(OnFileUploadCompleted);
@@ -41,5 +43,32 @@ public class UploadFiles
     void OnFileUploadCompleted(object sender, UploadFileCompletedEventArgs e)
     {
         Debug.Log("File Uploaded");
+    }
+
+    public async Task Upload(string ftpAddress, string filePath, string Username, string Password)
+    {
+        try
+        {
+            FtpWebRequest re = (FtpWebRequest)FtpWebRequest.Create(ftpAddress + "/UsersInfo/" + "User_" + "Baladam");
+            re.Method = WebRequestMethods.Ftp.UploadFile;
+            re.Credentials = new NetworkCredential(Username, Password);
+            re.UsePassive = true;
+            re.UseBinary = true;
+            re.KeepAlive = false;
+
+            FileStream stream = File.OpenRead(filePath);
+            byte[] buffer = new byte[stream.Length];
+            await stream.ReadAsync(buffer, 0, buffer.Length);
+            stream.Close();
+
+            Stream reqstream = re.GetRequestStream();
+            await reqstream.WriteAsync(buffer, 0, buffer.Length);
+            reqstream.Close();
+            Debug.Log("Uploaded");
+        }
+        catch
+        {
+            Debug.Log("Upload Error!!!");
+        }
     }
 }
